1d89123c11bcafd61212653a4f40ae2e4a980eb6
Marta Mozelle <marta.mozelle@gmail.com>
2020-12-23 18:58:41 -0500

    msg

--- src/server/index.js
+++ src/server/index.js
@@ -9,14 +9,14 @@ const port = process.env.PORT || 5000;
 
 app.use(morgan('dev'));
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '.../', 'build')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get('/', function (req, res) {
-  res.sendFile('index.html', { root: 'build/' }); // ENOENT: no such file or directory, stat '/app/app/build/index.html'
+  res.sendFile('index.html', { root: 'build/' }); // Just white, no error. 
   //res.sendFile(path.join(__dirname, 'index.html'));
   //res.sendFile('index.html'); // "path must be absolute or specify root to res.sendFile"
   // res.sendFile(path.resolve("build/index.html")) // nothing
