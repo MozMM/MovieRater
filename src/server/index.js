0253e7758506afa3bbb47a3cc4d4d602da410453
Marta Mozelle <marta.mozelle@gmail.com>
2020-12-23 18:08:43 -0500

    go

--- src/server/index.js
+++ src/server/index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join('index.html')) 
+  res.sendFile('index.html');
   //res.sendFile(path.join(__dirname, 'index.html'));
   //console.log('path is =>>', path.join(__dirname, '.../', 'build', 'index.html'));
 });
