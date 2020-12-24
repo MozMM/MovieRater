f225fc9be4ab4207ef07a3eefc217b0e54e772b6
Marta Mozelle <marta.mozelle@gmail.com>
2020-12-24 12:06:34 -0500

    build static 5

--- src/server/index.js
+++ src/server/index.js
@@ -9,19 +9,13 @@ const port = process.env.PORT || 5000;
 
 app.use(morgan('dev'));
 
-// app.use(express.static('../../build/static'));
-app.use(express.static('../../build/static'));
+app.use(express.static('./app/build/static'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', function (req, res) {
-  res.sendFile('index.html', { root: 'build/' });  // Just white, no error. reading index.html, and> cannot find manifest.json. Something wrong with %public% or static
-  // res.sendFile('index.html', { root: __dirname }); // ENOENT: no such file or directory, stat '/app/src/server/  index.html' 
-  //res.sendFile(path.join(__dirname, 'index.html'));
-  //res.sendFile('index.html'); // "path must be absolute or specify root to res.sendFile"
-  // res.sendFile(path.resolve("build/index.html")) // nothing
-  //console.log('path is =>>', path.join(__dirname, '.../', 'build', 'index.html'));
+  res.sendFile('index.html', { root: 'build/' });  
 });
 
 // to ratings routes
