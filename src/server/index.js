7f7203894fdb2ac56146586dad7b116eb0599d41
Alan Siegler <alan.siegler@equinoxmedia.com>
2020-12-24 16:27:49 -0500

    adding procfile + config updates

--- src/server/index.js
+++ src/server/index.js
@@ -1,26 +1,27 @@
 const express = require('express');
 const morgan = require('morgan');
-
 const bodyParser = require('body-parser')
 const path = require('path');
 const app = express();
 const db = require('../server/db')
 const port = process.env.PORT || 5000; 
 
+let basePath = path.join(__dirname, '../../build');
+
+if ( process.env.MODE !== 'development') {
+  basePath = path.join(__dirname, '../build');
+}
+
 app.use(morgan('dev'));
-
-app.use(express.static(path.join(__dirname, '.../', 'build')));
-
+app.use(express.static(basePath));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '.../', 'build', 'index.html'));
-});
-
 // to ratings routes
 app.use('/api', require('./api'))
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(basePath, '../../build/index.html'));
+});
 
 app.use(function (err, req, res, next) {
   console.error(err);
